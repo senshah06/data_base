8ccec1d831fe91fe1ffea9e3881ce8a3ee39990f
ssen <ssen@10.2.79.233>
2022-07-05 10:17:06 +0530

    First Commit

---
+++ Springboot_emp/src/test/java/net/javaguides/emp/SpringbootEmpApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.emp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
